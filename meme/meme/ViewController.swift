7a7f98e78492bf74fa97324dd5422fde2c3040ff
Bulana <Bulana@hal9000.zapper.local>
2019-04-22 13:47:33 +0200

    this is the project structure

---
+++ meme/meme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meme
+//
+//  Created by Bulana on 2019/04/22.
+//  Copyright © 2019 Bulana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
